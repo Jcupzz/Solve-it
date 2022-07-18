9b90e37a81c9fb625f480b082388008751ebe8f6
Jcupzz <jcupzz255@gmail.com>
2022-07-18 15:19:54 +0530

    Added firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:solve_it/Authentication/Register.dart';
 import 'package:solve_it/HomePage.dart';
+import 'package:solve_it/firebase_options.dart';
 import 'package:solve_it/pages/Detailed_View.dart';
 import 'package:solve_it/pages/VirtualCoins_View.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -18,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Material(child: Detailed_View()),
+      home: Material(child: Register()),
     );
   }
 }
